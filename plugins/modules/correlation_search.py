8bb0fff390c9ee07bee42d7d8916ba7890a7925c
Gonéri Le Bouder <goneri@lebouder.net>
2021-11-17 10:22:00 -0500

    DNM - TESTING
    Depends-On: https://github.com/ansible/ansible-zuul-jobs/pull/1223

--- plugins/modules/correlation_search.py
+++ plugins/modules/correlation_search.py
@@ -6,10 +6,6 @@
 # (c) 2018, Adam Miller (admiller@redhat.com)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import absolute_import, division, print_function
-
-__metaclass__ = type
-
 DOCUMENTATION = """
 ---
 module: correlation_search
